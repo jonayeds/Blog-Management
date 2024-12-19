d3d79c290b95f6d2c6deda9040f446ed8fa69de0
jonayeds <jonayeeds@gmail.com>
2024-12-19 12:27:48 +0600

    connected to mongoDB

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+exports.app = (0, express_1.default)();
+// parsers
+exports.app.use(express_1.default.json());
+exports.app.use((0, cors_1.default)());
+// routes
+exports.app.use("/api");
+exports.default = exports.app.get("/", (req, res) => {
+    res.send("Blog Management is running 🏃🏼‍♂️‍➡️");
+});
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express"
+import cors from "cors"
+
+export const app:Application = express()
+
+// parsers
+app.use(express.json())
+app.use(cors())
+
+// routes
+// app.use("/api")
+
+export default app.get("/",(req:Request, res:Response)=>{
+    res.send("Blog Management is running 🏃🏼‍♂️‍➡️")
+})
