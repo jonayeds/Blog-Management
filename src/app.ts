457e661ac3d828d8d825598c1c5812082f32a690
jonayeds <jonayeeds@gmail.com>
2024-12-20 17:55:11 +0600

    added not found route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { router } from './app/routes';
+import { globalErrorHandler } from './app/middleware/globalErrorHandler';
+import { notFound } from './app/middleware/notFound';
 
 export const app: Application = express();
 
@@ -11,6 +13,14 @@ app.use(cors());
 // routes
 app.use("/api", router)
 
+// Test route
 export default app.get('/', (req: Request, res: Response) => {
   res.send('Blog Management is running 🏃🏼‍♂️‍➡️');
 });
+
+// Global Error Handler
+app.use(globalErrorHandler)
+
+
+// Not Found Route
+app.use(notFound)
